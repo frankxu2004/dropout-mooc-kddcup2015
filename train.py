b9dbf5371f0d67617a897c40f22650aced15d631
reversefelicity <yuandaxia37@sjtu.edu.cn>
2015-06-02 13:00:45 +0800

    Revert "Revert "update""
    This reverts commit 869cbbd82a2bb16d0cdf4275c90c3eb2801eb6ac.

--- train.py
+++ train.py
@@ -3,11 +3,18 @@ import csv
 import numpy as np
 import pandas as pd
 import time
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn import svm
 from sklearn.svm import LinearSVR
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+from sklearn.decomposition import PCA
 
 def readCSV( path ):
     csvfile = file(path,'rb')
@@ -54,35 +61,49 @@ def putTestIntolist(reader):
         for i in line[1:]:
             row.append( float(i) )
         l.append(row)
+    print "finish putting"
     return l
 
-def machineLearning(X_parameters,Y_parameters,  predict_value):
-    clf1 = LinearSVR()   # svm for regression
-    clf2 = LinearRegression() 
-    clf1.fit(X_parameters, Y_parameters)
-    clf2.fit(X_parameters, Y_parameters)
+def protect (a):
+    if a>1:
+        return 1
+    if a<0:
+        return 0
+    return a
+
+def machineLearning(X, Y_parameters,  predict_value, writer):
+    X_parameters = X
+    clf1 = LinearSVR()
+    clf2 = LinearRegression()
+    clf3 = RandomForestClassifier()
+    clf4 = LogisticRegression()
+    clf5 = DecisionTreeClassifier()
+    clf6 = GradientBoostingClassifier()
+    ##clf1.fit(X_parameters, Y_parameters)
+    #clf2.fit(X_parameters, Y_parameters)
+    #clf3.fit(X_parameters, Y_parameters)
+    clf4.fit(X_parameters, Y_parameters)
+    #clf5.fit(X_parameters, Y_parameters)
+    clf6.fit(X_parameters, Y_parameters)
     print "finish fitting"
     answer = []
-    max = 0
-    min = 1
     for line in predict_value:
         line1 = line[1:]
-        predict_outcome1 = clf1.predict(line1)
-        predict_outcome2 = clf2.predict(line1)
-        # 数据要求 [0,1]
-        if predict_outcome1[0]>1:
-            predict_outcome1[0]=1
-        if predict_outcome1[0]<0:
-            predict_outcome1[0]=0
-        if predict_outcome2[0]>1:
-            predict_outcome2[0]=1
-        if predict_outcome2[0]<0:
-            predict_outcome2[0]=0
-        data = [line[0],(predict_outcome1[0]+predict_outcome2[0])*0.5]
-        answer.append(data)
+        #predict_outcome1 = clf1.predict(line1)
+        #predict_outcome2 = clf2.predict(line1)
+        #predict_outcome3 = clf3.predict_proba(line1)
+        predict_outcome4 = clf4.predict_proba(line1)
+        #predict_outcome5 = clf5.predict_proba(line1)
+        predict_outcome6 = clf6.predict_proba(line1)
+        #value1 = predict_outcome1[0]
+        #value2 = predict_outcome2[0]
+        #value3 = predict_outcome3[0][1]
+        value4 = predict_outcome4[0][1]
+        #value5 = predict_outcome5[0][1]
+        value6 = predict_outcome6[0][1]
+        data =  (value4+value6)/2
+        writer.writerow([line[0],data])
     print "finish learning"
-    return answer, max,min
-    
     
 if __name__ == '__main__':
     start = time.time()
@@ -93,10 +114,8 @@ if __name__ == '__main__':
     x_parameter = putXIntolist( csv.reader(train_x) )
     y_parameter = putYIntolist( csv.reader(train_y) )
     test_parameter = putTestIntolist( csv.reader(test) )
-    ans,ma,mi = machineLearning(x_parameter,y_parameter, test_parameter)
-    #ans = mapp(ans,ma,mi)
-    for i in ans:
-        (csv.writer(outcome)).writerow(i)
+    
+    machineLearning(x_parameter,y_parameter, test_parameter, csv.writer(outcome))
     train_x.close()
     train_y.close()
     test.close()
